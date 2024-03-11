4b69354aa78771adda452a263653c7f51190a8c1
Dumitru Colesnicov <158239672+DimaCol90@users.noreply.github.com>
2024-03-11 15:09:37 +0200

    Add files via upload

---
+++ L_16_aritm_func_hw_done.py
@@ -0,0 +1,18 @@
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+#rewrite r=1+2*3/4
+
+r=mul(10,100)
+print(r)
+
+r=add(1,(mul(2,div(3,4))))
